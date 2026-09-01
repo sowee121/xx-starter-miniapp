93276d1c77d27bd9a55dfe3d6ea9394d1f1c0953
sowee121 <18671659+sowee121@users.noreply.github.com>
2026-09-01 14:27:14 +0800

    style(shop): 调整贴纸主题色与吉祥物渐变色

--- miniprogram/subpkg/shop/content/stickers.js
+++ miniprogram/subpkg/shop/content/stickers.js
@@ -1,20 +1,38 @@
 const { mediaUrl } = require('../../../config/media')
 
+// [名称, id, 消耗星星, 主题色]
+// 主题色取自项目低饱和护眼 tone-* 体系，按动物色系分组、相邻不撞色，每种动物固定专属
 const ANIMALS = [
-  ['小兔', 'rabbit', 2], ['小猫', 'cat', 2], ['小狗', 'dog', 2], ['小鸭', 'duckling', 2],
-  ['小鸡', 'chicken', 2], ['小猪', 'pig', 2], ['小鸟', 'bird', 2], ['小鱼', 'fish', 2],
-  ['小仓鼠', 'hamster', 2], ['小熊', 'bear', 4], ['小企鹅', 'penguin', 4], ['小羊', 'sheep', 4],
-  ['小牛', 'cow', 4], ['小猴子', 'monkey', 4], ['小狐狸', 'fox', 6], ['小熊猫', 'panda', 6],
-  ['小象', 'elephant', 6], ['长颈鹿', 'giraffe', 6], ['小海豚', 'dolphin', 6], ['小水獭', 'otter', 6],
-  ['小恐龙', 'dino', 8], ['小狮子', 'lion', 8], ['小老虎', 'tiger', 8], ['独角兽', 'unicorn', 10],
+  ['小兔', 'rabbit', 2, 'rose'],
+  ['小猫', 'cat', 2, 'peach'],
+  ['小狗', 'dog', 2, 'apricot'],
+  ['小鸭', 'duckling', 2, 'butter'],
+  ['小鸡', 'chicken', 2, 'apricot'],
+  ['小猪', 'pig', 2, 'rose'],
+  ['小鸟', 'bird', 2, 'sky'],
+  ['小鱼', 'fish', 2, 'mint'],
+  ['小仓鼠', 'hamster', 2, 'peach'],
+  ['小熊', 'bear', 4, 'apricot'],
+  ['小企鹅', 'penguin', 4, 'sky'],
+  ['小羊', 'sheep', 4, 'cream'],
+  ['小牛', 'cow', 4, 'matcha'],
+  ['小猴子', 'monkey', 4, 'apricot'],
+  ['小狐狸', 'fox', 6, 'butter'],
+  ['小熊猫', 'panda', 6, 'lilac'],
+  ['小象', 'elephant', 6, 'sky'],
+  ['长颈鹿', 'giraffe', 6, 'butter'],
+  ['小海豚', 'dolphin', 6, 'mint'],
+  ['小水獭', 'otter', 6, 'sky'],
+  ['小恐龙', 'dino', 8, 'matcha'],
+  ['小狮子', 'lion', 8, 'apricot'],
+  ['小老虎', 'tiger', 8, 'butter'],
+  ['独角兽', 'unicorn', 10, 'lilac'],
 ]
 
-const TONES = ['rose', 'sky', 'butter', 'matcha', 'peach', 'lilac', 'mint', 'apricot']
-
-module.exports = ANIMALS.map(([name, id, cost], index) => ({
+module.exports = ANIMALS.map(([name, id, cost, tone]) => ({
   id,
   name,
   cost,
-  tone: TONES[index % TONES.length],
+  tone,
   image: mediaUrl(`/subpkg/shop/static/sticker-${id}.png`),
 }))
